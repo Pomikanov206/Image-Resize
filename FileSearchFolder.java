9a9991a426de2caf32bd4f7ebff2ee2dea2353a7
Pomikanov206 <pomikanov@gmail.com>
2017-08-17 16:47:39 +0300

    Refactor load system

---
+++ FileSearchFolder.java
@@ -0,0 +1,37 @@
+package resize;
+
+import java.io.File;
+
+public class FileSearchFolder {
+
+    private String pathFolder;
+    private File folder;
+
+    public FileSearchFolder(String pathFolder){
+        this.pathFolder = pathFolder;
+        folder = new File(pathFolder);
+    }
+
+    public String[] getFiles(){
+
+        File[] files = folder.listFiles();
+        String[] pathFiles = null;
+
+        try {
+            pathFiles = new String[files.length];
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println(e.getStackTrace());
+            return null;
+        }
+
+        for(int i = 0; i < files.length; i++)
+        {
+
+            pathFiles[i] = files[i].getPath();
+        }
+
+        return pathFiles;
+    }
+}
--- ImgOpen.java
+++ ImgOpen.java
@@ -13,10 +13,16 @@ import java.util.regex.Pattern;
 public class ImgOpen {
 
     //private static final String FORMAT_IMG_MASK = "^.*\\.[a-zA-Z]{3,4}";
-    private static final String FORMAT_IMG_MASK = "^.*\\.[(JPG)(jpg)(JPEG)(jpeg)]";
+    private static final String FORMAT_IMG_MASK = "^.*\\.[(jpg)(jpeg)]{3,4}";
+    private String path;
+
+    public ImgOpen(String path)
+    {
+        this.path = path.toLowerCase();
+    }
 
     //Создание файла и загрузка в него изображения
-    public static Image loadImage(String path){
+    public Image loadImage(){
         //Новое изображение
 
         if(!isImage(path))
@@ -35,44 +41,44 @@ public class ImgOpen {
     }
 
     //Создание файлов и загрузка в них изображений
-    public static Image[] loadArrayImages(String pathFolder)
-    {
-        //Определение папки
-        File folder = new File(pathFolder);
-        //Получение списка имен файлов в папке
-        String[] arrayFilesNames = folder.list();
-
-        //Получение списка имен изображений в папке
-        ArrayList<String> arrayImageNames = new ArrayList<>();
-
-        for(int i = 0; i < arrayFilesNames.length; i++)
-        {
-            if(isImage(arrayFilesNames[i]))
-                arrayImageNames.add(arrayFilesNames[i]);
-        }
-
-        //Массив новых пустых файлов
-        Image[] arrayImage = new Image[arrayImageNames.size()];
-
-        //Заполняшка
-        for(int i = 0; i < arrayImageNames.size(); i++){
-
-            StringBuffer path = new StringBuffer();
-
-            path.append(pathFolder);
-            path.append(arrayImageNames.get(i));
-
-            if(!isImage(path.toString()))
-            {
-                System.out.println("Error Image");
-                continue;
-            }
-
-            arrayImage[i] = loadImage(path.toString());
-        }
-
-        return arrayImage;
-    }
+//    public Image[] loadArrayImages(String pathFolder)
+//    {
+//        //Определение папки
+//        File folder = new File(pathFolder);
+//        //Получение списка имен файлов в папке
+//        String[] arrayFilesNames = folder.list();
+//
+//        //Получение списка имен изображений в папке
+//        ArrayList<String> arrayImageNames = new ArrayList<>();
+//
+//        for(int i = 0; i < arrayFilesNames.length; i++)
+//        {
+//            if(isImage(arrayFilesNames[i]))
+//                arrayImageNames.add(arrayFilesNames[i]);
+//        }
+//
+//        //Массив новых пустых файлов
+//        Image[] arrayImage = new Image[arrayImageNames.size()];
+//
+//        //Заполняшка
+//        for(int i = 0; i < arrayImageNames.size(); i++){
+//
+//            StringBuffer path = new StringBuffer();
+//
+//            path.append(pathFolder);
+//            path.append(arrayImageNames.get(i));
+//
+//            if(!isImage(path.toString()))
+//            {
+//                System.out.println("Error Image");
+//                continue;
+//            }
+//
+//            arrayImage[i] = loadImage(path.toString());
+//        }
+//
+//        return arrayImage;
+//    }
 
     private static boolean isImage(String name){
         Pattern pattern = Pattern.compile(FORMAT_IMG_MASK);
